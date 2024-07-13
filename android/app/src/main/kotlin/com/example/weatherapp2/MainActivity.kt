8a3d0fb67e8d8b97c91d22ef2fb2255174e087ae
Armel Bogue <komlanbogue@gmail.com>
2024-07-13 23:03:21 +0000

    "Updated README.md with Flutter project template content"

---
+++ android/app/src/main/kotlin/com/example/weatherapp2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.weatherapp2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:weatherapp2/View/Pages/splash.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Splash(),
+    );
+  }
+}
